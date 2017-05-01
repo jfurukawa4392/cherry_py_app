5e553386eeb621bf7cafd9bdf760a64c4130abb9
Jesse Furukawa <tuc64377@temple.edu>
2017-05-01 13:33:16 -0700

    set up webpack and react dependencies

--- query_app.py
+++ query_app.py
@@ -1,12 +1,15 @@
 import cherrypy
 import json
+import os, os.path
 
 class QueryResults(object):
 
     @cherrypy.expose
     def index(self):
         return """<html>
-          <head></head>
+          <head>
+            <link href="/static/css/style.scss" rel="stylesheet">
+          </head>
           <body>
             <form method="get" action="search">
               <input type="text" value="Search..." name="query" />
@@ -21,10 +24,20 @@ class QueryResults(object):
             data = json.load(data_file)
 
         if query in data.keys():
-            return data[query]
+            return json.dumps(data[query])
         else:
             return "error"
 
 
 if __name__ == '__main__':
-    cherrypy.quickstart(QueryResults())
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    cherrypy.quickstart(QueryResults(), '/', conf)
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './public/js/index.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
